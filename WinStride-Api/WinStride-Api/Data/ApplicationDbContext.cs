462004a75448aa8947117c523e13d8e58f935e99
Ofish <113127052+Ofish-Ofish@users.noreply.github.com>
2026-02-20 00:08:03 -0500

    removed useless comments

--- WinStride-Api/WinStride-Api/Data/ApplicationDbContext.cs
+++ WinStride-Api/WinStride-Api/Data/ApplicationDbContext.cs
@@ -1,17 +1,14 @@
 using Microsoft.EntityFrameworkCore;
-using WinStrideApi.Models; // This assumes your WinEvent class is in the Models folder
+using WinStrideApi.Models;
 
 namespace WinStrideApi.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        // This constructor is required so we can pass the connection string from Program.cs
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
-
-        // This creates your table in Postgres
         public DbSet<WinEvent> WinEvents { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
